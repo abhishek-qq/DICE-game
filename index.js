1bd8fc59ff790cf86f186439d454513277399aeb
Abhishek Mishra <63139522+abhishek-qq@users.noreply.github.com>
2021-08-23 11:05:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// creating a variable to store the value of the random number
+
+var randomNumber1= Math.floor((Math.random()*6 + 1));
+var newsrc= "images/"+"dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src", newsrc);
+
+
+var randomNumber2= Math.floor((Math.random()*6 + 1));
+var newsrc1= "images/"+"dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", newsrc1);
+
+if(randomNumber2===randomNumber1){
+	document.querySelector("h1").innerHTML="DRAW";
+
+}
+else if(randomNumber1>randomNumber2){document.querySelector("h1").innerHTML="Player1 is winner";
+
+}
+else{document.querySelector("h1").innerHTML="Player2 is winner";
+
+}
+
+
